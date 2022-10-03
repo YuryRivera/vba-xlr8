207982b635e5980d70cd028d19fdabb8f797a83c
Yury <yury.rivera@gaf.energy>
2022-10-02 23:00:42 -0600

    launching external process with folder cwd

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,44 @@
-import { commands, ExtensionContext, window } from "vscode";
+import { commands, ExtensionContext, window, workspace } from "vscode";
 import * as child from 'child_process';
 import * as path from 'path';
 
 
-const vbaRunningContext = 'vbaxrl8run';
+const vbaRunningContext = 'vbaxlr8run';
 let proccess: child.ChildProcess | undefined = undefined;
 
 export function activate(context: ExtensionContext) {
+	const folders = workspace.workspaceFolders;
+	if(!folders || folders.length < 1){
+		window.showErrorMessage("VBA XLR8 only works for valid project folder, please open the folder containing the vba.json file");
+		return;
+	}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vba-xlr8" is now active!');
+	const output = window.createOutputChannel("VBA xlr8");
+	const folder = folders[0];
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let suscription = commands.registerCommand('vba-xlr8.vbaRun', () => {
+	output.appendLine(`Working at ${folder.name}: ${folder.uri.fsPath}`);
+	output.appendLine(`Working at ${folder.uri.path}`);
+	
+
+	let suscription = commands.registerCommand('vba-xlr8.compile', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		if (proccess !== undefined) { return; }
 		commands.executeCommand('setContext', vbaRunningContext, true);
 		const compilerModule = context.asAbsolutePath(path.join('compiler', 'VBA.Compiler.exe'));
-		console.log(compilerModule);
 
 		proccess = child.execFile(
 			compilerModule,
-			['la', 'puta'],
+			['test', 'arguments'],
+			{
+				cwd: folder.uri.fsPath,
+			},
 			(error, stdout, stderr) => {
 				if (error) {
 					throw error;
+				}
+				if(stderr){
+					console.log(stderr);
 				}
 				const editor = window.activeTextEditor;
 				if (editor) {
@@ -36,11 +46,10 @@ export function activate(context: ExtensionContext) {
 						e.insert(editor.selection.active, stdout);
 					});
 				}
-				console.log(stdout);
+				output.appendLine(stdout);
 			});
 
 		proccess!.on('exit', (code) => {
-			console.log('process exit with code: ' + code);
 			proccess = undefined;
 			commands.executeCommand('setContext', vbaRunningContext, false);
 		});
@@ -59,4 +68,7 @@ export function activate(context: ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {
+	proccess?.kill();
+	proccess = undefined;
+ }
